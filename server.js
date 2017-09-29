de77bbdf29130160a04ae4352a4d79714d02bae1
sumiranraut <sumiranraut96@gmail.com>
2017-09-29 22:37:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/aticle-one',function (req, res) {
+app.get('/Article-one',function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
 });
 
-app.get('/aticle two',function (req, res) {
+app.get('/Article two',function (req, res) {
   res.send('Article two requested and will be served here');  
 });
 
-app.get('/aticle three',function (req, res) {
+app.get('/Article three',function (req, res) {
   res.send('Article three requested and will be served here');  
 });
 
